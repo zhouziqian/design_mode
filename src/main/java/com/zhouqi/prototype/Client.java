f5305bd0296e99e2a3f38caf0777a24954e948e5
zhouqi <281990965@qq.com>
2017-10-11 17:44:26 +0800

    原型模式

---
+++ src/main/java/com/zhouqi/prototype/Client.java
@@ -0,0 +1,13 @@
+package com.zhouqi.prototype;
+
+public class Client {
+    public static void main(String[] args){
+        ConcretePrototype cp = new ConcretePrototype();
+//        for(int i=0; i< 10; i++){
+//            ConcretePrototype clonecp = (ConcretePrototype)cp.clone();
+//            clonecp.show();
+//        }
+        ConcretePrototype clonecp = (ConcretePrototype)cp.clone();
+        System.out.println(cp==clonecp);
+    }
+}
---
+++ src/main/java/com/zhouqi/prototype/ConcretePrototype.java
@@ -0,0 +1,7 @@
+package com.zhouqi.prototype;
+
+public class ConcretePrototype extends Prototype {
+    public void show(){
+        System.out.println("Prototype mode impl");
+    }
+}

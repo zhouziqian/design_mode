8504bce584eb5e6c9b2a5ce6015caa8cbd3d7b7e
zhouqi <281990965@qq.com>
2017-10-12 16:57:22 +0800

    模板模式

---
+++ src/main/java/com/zhouqi/template/Client.java
@@ -0,0 +1,11 @@
+package com.zhouqi.template;
+
+public class Client {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.prepareGotoSchool();
+
+        Teacher teacher  = new Teacher();
+        teacher.prepareGotoSchool();
+    }
+}
